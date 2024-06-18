3f0aef93f7808b8f2e7ab3485c8290a7281471d7
Daniel Oleksiyenko <51185857+doleksiyenko@users.noreply.github.com>
2024-06-18 14:39:10 -0400

    new class files + add 8 KiB ram + vram

--- include/ppu.h
+++ include/ppu.h
@@ -1,8 +1,12 @@
 #ifndef PPU_H
 #define PPU_H
 
-class PPU {
+#include <cstdint>
+#include <array>
 
+class PPU {
+    public:
+        std::array<uint8_t, 1024 * 8> vram;
 };
 
 #endif
--- include/ram.h
+++ include/ram.h
@@ -1,8 +1,12 @@
 #ifndef RAM_H
 #define RAM_H
 
-class RAM {
+#include <array>
+#include <cstdint>
 
+class RAM {
+    public:
+        std::array<uint8_t, 1024 * 8> ram;
 };
 
 #endif
