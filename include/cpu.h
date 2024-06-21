40104fd99a5fd261a495ea1f8a4fa7b0370e0ebe
doleksiyenko <dan.oleksiyenko@gmail.com>
2024-06-21 09:14:35 -0400

    define cpu registers

--- include/cpu.h
+++ include/cpu.h
@@ -1,8 +1,22 @@
 #ifndef CPU_H
 #define CPU_H
 
-class CPU {
+#include <cstdint>
 
+class CPU {
+    public:
+        CPU();
+        void fetch();
+        void execute();
+
+    private:
+        // 16 bit registers
+        uint16_t pc_; // program counter
+        uint16_t sp_; // stack pointer
+        uint16_t af_; // accumulator + flags
+        uint16_t bc_; uint16_t de_; uint16_t hl_;
+
+        // opcodes
 };
 
 #endif
