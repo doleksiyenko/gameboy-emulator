c12cd932d99406e2b86f073bee38e8dbec8def02
doleksiyenko <dan.oleksiyenko@gmail.com>
2024-06-22 21:42:41 -0400

    add load instruction definitions

--- include/cpu.h
+++ include/cpu.h
@@ -17,6 +17,9 @@ class CPU {
         uint16_t bc_; uint16_t de_; uint16_t hl_;
 
         // opcodes
+        uint8_t nop(); uint8_t ldrr(); uint8_t ldrn(); uint8_t ldrhl();
+        uint8_t ldr(); uint8_t ldhlr(); uint8_t ldhln(); uint8_t ldabc();
+        uint8_t ldade(); uint8_t ldbca(); uint8_t lddea(); uint8_t ldann();
 };
 
 #endif
