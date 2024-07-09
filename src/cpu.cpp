445210b21bafe2f878b0228dfe78ef3fe60db0d5
doleksiyenko <dan.oleksiyenko@gmail.com>
2024-07-09 13:35:11 -0400

    XOR instructions

--- src/cpu.cpp
+++ src/cpu.cpp
@@ -1052,14 +1052,36 @@ uint8_t CPU::AND_A()
     return 0;
 }
 
-uint8_t CPU::XOR_B() {}
-uint8_t CPU::XOR_C() {}
-uint8_t CPU::XOR_D() {}
-uint8_t CPU::XOR_E() {}
-uint8_t CPU::XOR_H() {}
-uint8_t CPU::XOR_L() {}
-uint8_t CPU::XOR_HL_m() {}
-uint8_t CPU::XOR_A() {}
+void CPU::XOR(uint8_t reg_content)
+{
+    uint16_t result = ((af_ & 0xff00) >> 8) ^ (reg_content);
+
+    if (result == 0) {
+        set_flag(CPU::flags::Z, 1);
+    }
+    else {
+        set_flag(CPU::flags::Z, 0);
+    }
+
+    set_flag(CPU::flags::N, 0);
+    set_flag(CPU::flags::H, 0);
+    set_flag(CPU::flags::C, 0);
+
+    af_ &= 0xff;
+    af_ |= result << 8;
+}
+
+uint8_t CPU::XOR_B() { XOR((bc_ & 0xff00) >> 8); return 0; }
+uint8_t CPU::XOR_C() { XOR((bc_ & 0xff)); return 0; }
+uint8_t CPU::XOR_D() { XOR((de_ & 0xff00) >> 8); return 0; }
+uint8_t CPU::XOR_E() { XOR((de_ & 0xff)); return 0; }
+uint8_t CPU::XOR_H() { XOR((hl_ & 0xff00) >> 8); return 0; }
+uint8_t CPU::XOR_L() { XOR((hl_ & 0xff)); return 0; }
+uint8_t CPU::XOR_HL_m() { XOR(read(hl_)); return 0; }
+uint8_t CPU::XOR_A() { XOR((af_ & 0xff00) >> 8); return 0; }
+
+
+
 uint8_t CPU::OR_B() {}
 uint8_t CPU::OR_C() {}
 uint8_t CPU::OR_D() {}
