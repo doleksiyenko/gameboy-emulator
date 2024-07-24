5deecd93ef4509ee30d89b548333fa9e22069764
doleksiyenko <dan.oleksiyenko@gmail.com>
2024-07-24 11:49:12 -0400

    Add a check for if the cartridge size is valid

--- include/cartridge.h
+++ include/cartridge.h
@@ -8,8 +8,10 @@
 class Cartridge {
     public:
         void load_cartridge_from_file(std::string cartridge_file);
+        uint8_t read(uint16_t address);
     private:
         std::vector<uint8_t> cartridge_; // store the contents of the cartridge into a vector - since this might be variable length with different MBCs, this may be different sizes
+        uint8_t mbc_; // MBC (memory bank controller) mode of the cartridge
 };
 
 #endif
--- src/cartridge.cpp
+++ src/cartridge.cpp
@@ -24,4 +24,16 @@ void Cartridge::load_cartridge_from_file(std::string cartridge_file)
     }
 
     cartridge_reader.close();
+
+    // to be a valid cartridge, the number of bytes must be divisible by the bank size, 16KB
+    if (cartridge_.size() % (16 * 1024) != 0) {
+        std::cout << "Error: invalid cartridge size." << std::endl;
+        exit(-1);
+    }
+
+    // read any required information from the cartridge header
+    mbc_ = cartridge_.at(0x0147); // mbc mode found in catridge header. 0x0147 contains the cartridge type, and indicates how the Cartridge is organized
+
+    std::cout << "MBC type: " << (int)mbc_ << '\n'; 
+    std::cout << "Cartridge size (bytes): " << cartridge_.size() << std::endl;
 }
