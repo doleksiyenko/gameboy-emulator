9ae4960a9500a72003a3fa10b7ddd3a9027a4fa7
Daniel Oleksiyenko <51185857+doleksiyenko@users.noreply.github.com>
2024-07-25 13:47:42 -0400

    add MBC control registers

--- include/mbc/mbc.h
+++ include/mbc/mbc.h
@@ -14,8 +14,16 @@ class MBC
 
         // given the address, read from different banks / external RAM depending on the MBC. Base class returns invalid read
         virtual uint8_t read(uint16_t address) { return 0xff; }; 
+        virtual void write(uint16_t address, uint8_t value) {};  // write to different MBC registers
+
     protected:
         std::vector<uint8_t> cartridge_data_;
+
+        // possible registers required by MBCs. All registers default to 0x00 on power up
+        bool ram_enable_ = false; // RAM enable needs to be activated first by writing $A to memory
+        uint8_t rom_bank_number_ = 0x0; 
+        uint8_t ram_bank_number_ = 0x0; 
+        uint8_t banking_mode_ = 0x0;
 };
 
 #endif
--- include/mbc/mbc1.h
+++ include/mbc/mbc1.h
@@ -8,7 +8,8 @@ class MBC1 : public MBC
 {
     public:
         MBC1(std::vector<uint8_t> cartridge) : MBC(cartridge) {};
-        uint8_t read(uint16_t address); // overriden function. Read the byte at the address in the cartridge based on the behaviour of MBC1
+        uint8_t read(uint16_t address) override; // Read the byte at the address in the cartridge based on the behaviour of MBC1
+        void write(uint16_t address, uint8_t value) override; // write to the MBC registers
 };
 
 #endif
